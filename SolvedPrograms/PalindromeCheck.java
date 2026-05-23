aeb6c8419bae12a1c3df68be8cacffabbaf87520
Madhu <madhuthota131@gmail.com>
2026-05-23 09:12:28 +0530

    Java Programs

---
+++ SolvedPrograms/PalindromeCheck.java
@@ -0,0 +1,28 @@
+// Problem: Palindrome checking
+// Approach: Two Pointer Technique
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+      String str="A man a plan a canal Panama";
+      str = str.replace(" ", "");
+      str=str.toLowerCase();
+      int start=0;
+      int end=str.length()-1;
+      boolean isPalindrome=true;
+      while(start<end)
+      {
+        if(str.charAt(start)!=str.charAt(end))
+        {
+          isPalindrome=false;
+          break;
+        }
+        start++; end--;
+      }
+      if(isPalindrome)
+          System.out.print("palindrome");
+      else
+          System.out.print("not a palindrome");
+    }
+}
